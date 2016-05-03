0dd881113b370d91ad1c69b4e2a0b7461d07ed28
Ron Emrick <ronemrick@gmail.com>
2016-05-03 11:59:17 -0400

    contact info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,12 +9,36 @@ This is empty on purpose! Your code to build the resume will go here.
 //var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN!");
 //$("#main").append(funThoughts);
 
-var name = "Ron Emrick";
-var formattedName = HTMLheaderName.replace("%data%", name);
+var skills = ["programming", "teaching", "Javascript"];
 
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var bio = {
+	"name" : "Ron Emrick",
+	"role" : "Web Developer",
+	"contactInfo" : {
+		"mobile" : "555-123-4567",
+		"email" : "ronemrick@gmail.com",
+		"twitter" : "@ronemrick",
+		"github" : "ronemrick",
+		"location" : "New Hampshire"
+	},
+	"pictureURL" : "images/biophoto.jpg",
+	"message" : "Lorem ipsum dolor sit amet, ne etiam dissentias mel, id saepe noster contentiones nam!",
+	"skills" : skills
+};
 
+//var name = "Ron Emrick";
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+
+//var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.message);
+var formattedPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
+
+$("#header").prepend(formattedMobile);
+$("#header").prepend(formattedPic);
+$("#header").prepend(formattedMessage);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
