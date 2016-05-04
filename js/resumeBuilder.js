ca9ea3a52c661090019290b6a73c063ad3f3156f
Ron Emrick <ronemrick@TITAN.local>
2016-05-04 08:05:05 -0400

    clean up

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,79 +1,68 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-//$("#main").append("Ron");
-
-//var awesomeThoughts = "I am Ron and I am AWESOME!";
-//console.log(awesomeThoughts);
-
-//var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN!");
-//$("#main").append(funThoughts);
 
 var skills = ["programming", "teaching", "Javascript"];
 
+//
 // Declared using Object Literal Notation (JSON)
+//
 var bio = {
 	"name" : "Ron Emrick",
 	"role" : "Web Developer",
-	"contactInfo" : {
+	"contacts" : {
 		"mobile" : "555-123-4567",
 		"email" : "ronemrick@gmail.com",
 		"twitter" : "@ronemrick",
 		"github" : "ronemrick",
 		"location" : "New Hampshire"
 	},
-	"pictureURL" : "images/biophoto.jpg",
-	"message" : "Lorem ipsum dolor sit amet, ne etiam dissentias mel, id saepe noster contentiones nam!",
+	"bioPic" : "images/biophoto.jpg",
+	"welcomeMessage" : "Lorem ipsum dolor sit amet, ne etiam dissentias mel, id saepe noster contentiones nam!",
 	"skills" : skills
 };
 
-//bio.city = "Windham";  // add new property
-bio["city"] = "Windham";
-
-//var name = "Ron Emrick";
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-//var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.message);
-var formattedPic = HTMLbioPic.replace("%data%", bio.pictureURL);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
-var formattedGitHub = HTMLgithub.replace("%data%", bio.contactInfo.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.city + ", " + bio.contactInfo.location);
-//var formattedLocation = HTMLlocation.replace("%data%", bio.city);
-
-//$("#main").append(bio.city);
-
-$("#header").prepend(formattedLocation);
-$("#header").prepend(formattedGitHub);
-$("#header").prepend(formattedTwitter);
-$("#header").prepend(formattedEmail);
-$("#header").prepend(formattedMobile);
-$("#header").prepend(formattedPic);
-$("#header").prepend(formattedMessage);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
+//
 // Declared using Object Dot Notation
-var work = {};
-work.title = "Software Developer";
-work.employer = "Wasabi Ventures, LLC";
-work.numYears = 2;
-work.city = "Manchester, NH";
+//
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Wasabi Ventures, LLC",
+			"title" : "Director of Engineering Innovation",
+			"location" : "Manchester, NH",
+			"dates" : "2015-Present",
+			"description" : "Lorem ipsum dolor sit amet, ne etiam dissentias mel, id saepe noster contentiones nam"
+		},
+		{
+			"employer" : "ApplyKit, Inc.",
+			"title" : "CTO",
+			"location" : "Manchester, NH",
+			"dates" : "2014",
+			"description" : "Lorem ipsum dolor sit amet, ne etiam dissentias mel, id saepe noster contentiones nam"
+		}
+	]
+};
 
-$("#main").append(work["title"]);
-
-// Declared using Object Bracket Notation
-var education = {};
-education["schoolName"] = "Central CT State University";
-education["yearsAttended"] = "1986-1990";
-education["city"] = "New Britain, CT";
-
-$("#main").append(education.schoolName);
+var projects = {
+	"projects" : [
+		{
+			"title" : "Cool Project 1",
+			"dates" : "2015-Present",
+			"description" : "Lorem ipsum dolor sit amet, ne etiam dissentias mel, id saepe noster contentiones nam",
+			"images" : ["images/biophoto.jpg", "images/biophoto.jpg"]
+		},
+		{
+			"title" : "Cool Project 2",
+			"dates" : "2014",
+			"description" : "Lorem ipsum dolor sit amet, ne etiam dissentias mel, id saepe noster contentiones nam",
+			"images" : ["images/biophoto.jpg", "images/biophoto.jpg"]
+		}
+	]
+};
 
 // Declared using Object Literal Notation (JSON)
-var education2 = {
+var education = {
 	"schools" : [
 		{
 			"name" : "Central Connecticut State University",
@@ -103,7 +92,3 @@ var education2 = {
 		}
 	]
 }
-
-
-
-
